96b7e3fb0ab86a37f7ee835bcc8d614acf0986ea
Rene Ruprecht <rene.ruprecht.396@gmail.com>
2022-07-13 15:07:01 +0200

    Add: mongodb server connection

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// database
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://localhost/swipe-and-cook-db');
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.log(error);
+});
+
+db.once('open', () => {
+    console.log('connected');
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
